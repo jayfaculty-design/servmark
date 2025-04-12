71523f90416e9affa59e99b1641b48e87f0f3971
Jay Designs <izfaculty12@gmail.com>
2025-04-12 15:31:10 +0000

    changes made to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,9 @@ import HomeIndustries from "@/components/HomeIndustries";
 import HomeServices from "@/components/HomeServices";
 import TestimonialsCarousel from "@/components/Testimonials";
 import FeatureSection from "@/components/WhyChooseUs";
-import { Space_Grotesk } from "@next/font/google";
+
 import { Metadata } from "next";
 
-const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin", "latin-ext"],
-});
 export const metadata: Metadata = {
   title:
     "Servmark - Ghana's number one expert solution to professional services",
@@ -18,7 +15,7 @@ export const metadata: Metadata = {
 };
 export default function Home() {
   return (
-    <div style={{ fontFamily: spaceGrotesk.style.fontFamily }}>
+    <div>
       <Hero />
       <HomeAbout />
       <HomeIndustries />
